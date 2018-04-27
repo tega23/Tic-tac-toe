ca4f08ce63b63afb543504193a72c45bffd66317
Erekata Tega <tega23@yahoo.com>
2018-04-27 15:24:18 +0100

    Style changes

--- src/index.js
+++ src/index.js
@@ -80,9 +80,7 @@ class Game extends React.Component {
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         const moves = history.map((step, move) => {
-            const desc = move ?
-              'Go to move #' + move :
-              'Go to game start';
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
             return (
               <li key = {move} >
                 <button onClick={() => this.jumpTo(move)}>{desc}</button>
